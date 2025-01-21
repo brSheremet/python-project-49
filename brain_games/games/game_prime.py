18a71b6df31942137798f264bd7a4804776f77f4
Sheremet Bogdan <br.sheremet@gmail.com>
2025-01-21 20:54:27 +0300

    Исправил синтаксические ошибки

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -1,16 +1,17 @@
 import random
 from brain_games.cli import welcome_user
 
+
 def is_prime(num):
-    if num < 2: #Число меньше 2 не являются простыми
+    if num < 2:  # Число меньше 2 не являются простыми
         return False
     if num in (2, 3):
         return True
-    if num % 2 == 0 or num % 3 == 0: #Исключаем кратные 2 и 3
+    if num % 2 == 0 or num % 3 == 0:  # Исключаем кратные 2 и 3
         return False
     i = 5
     while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0: #Делится без остатка - не простые
+        if num % i == 0 or num % (i + 2) == 0:  # Делится без остатка - не простые
             return False
         i += 6
     return True
@@ -27,13 +28,13 @@ def play_is_prime():
         print(f"Question: {num}")
         user_answer = input("Your answer: ").strip().lower()
         
-
         correct_answer = "yes" if is_prime(num) else "no"
 
         if user_answer == correct_answer:
             print("Correct!")
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  + f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,37 +1,38 @@
 import random
 from brain_games.cli import welcome_user
 
+
 def generate_progression():
-    start = random.randint(1, 100) #Начало прогрессии
-    step = random.randint(1, 10) #Шаг 
-    length = 10 #Длина прогрессии согласно заданию
-    progression = [start + step * i for i in range(length)] #генерация прогресии
+    start = random.randint(1, 100)  # Начало прогрессии
+    step = random.randint(1, 10)    # Шаг
+    length = 10                      # Длина прогрессии согласно заданию
+    progression = [start + step * i for i in range(length)]  # Генерация прогрессии
 
-    missing_index = random.randint(0, length -1) #случайный индекс для прогрессии числа
-    missing_value = progression[missing_index] #значение которое будет пропущено
+    missing_index = random.randint(0, length - 1)  # Случайный индекс для прогрессии числа
+    missing_value = progression[missing_index]      # Значение, которое будет пропущено
 
-    progression[missing_index] = '..' #замена числа на двоеточие
+    progression[missing_index] = '..'  # Замена числа на двоеточие
 
     return progression, missing_value
 
 def ask_progression():
-  print("Welcome to the Brain Games!")
-  name = input("May I have your name? ")
-  print(f"Hello, {name}!")
-  print("What number is missing in the progression?")
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print("What number is missing in the progression?")
 
-  rounds =3
-  for _ in range(rounds):
-     progression, missing_value = generate_progression()
-     progression_str = ' '.join(map(str, progression)) #преобразование прогрессии в строку
-     print(f"Question: {progression_str}")
-     
-     user_answer = int(input("Your answer: "))
-     if user_answer == missing_value:
-        print("Correct!")
-     else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{missing_value}'.")
-        print(f"Let's try again, {name}!")
-        return
-      
-  print(f"Congratulations, {name}!")
+    rounds = 3  # Количество раундов
+    for _ in range(rounds):
+        progression, missing_value = generate_progression()
+        progression_str = ' '.join(map(str, progression))  # Преобразование прогрессии в строку
+        print(f"Question: {progression_str}")
+        
+        user_answer = int(input("Your answer: "))
+        if user_answer == missing_value:
+            print("Correct!")
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{missing_value}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
