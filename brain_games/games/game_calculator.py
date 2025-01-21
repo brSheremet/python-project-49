9743a49038e5b409390a2e852047a6fed28840bf
Sheremet Bogdan <br.sheremet@gmail.com>
2025-01-21 20:25:53 +0300

    Исправил синтаксические ошибки

--- brain_games/games/game_calculator.py
+++ brain_games/games/game_calculator.py
@@ -29,7 +29,8 @@ def play_calculator_game():
         if answer == correct_answer:
             print("Correct!")
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer ;(. " 
+                  + f"Correct answer was '{correct_answer}'.") 
             print(f"Let's try again, {name}!")
             break
     else:
