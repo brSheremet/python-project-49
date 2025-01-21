a73ac73a289f575c24e74cb1ee5d989cced8bb3a
Sheremet Bogdan <br.sheremet@gmail.com>
2025-01-21 20:35:10 +0300

    Исправил синтаксические ошибки gcd

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,30 +1,34 @@
 import random
 from brain_games.cli import welcome_user
 
+
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
+
 def brain_gcd():
-  print("Welcome to the Brain Games!")
-  name = input("May I have your name? ")
-  print(f"Hello, {name}!")
-  print("Find the greatest common divisor of given numbers.")
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print("Find the greatest common divisor of given numbers.")
   
-  for _ in range(3):
-    num1 = random.randint(1, 100)
-    num2 = random.randint(1, 100)
-    print(f"Question: {num1} {num2}")
+    for _ in range(3):
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        print(f"Question: {num1} {num2}")
     
-    correct_answer = gcd(num1, num2)
+        correct_answer = gcd(num1, num2)
     
-    answer = int(input("Your answer: "))
+        answer = int(input("Your answer: "))
     
-    if answer == correct_answer:
-      print("Correct!")
-    else:
-      print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-      print(f"Let's try again, {name}!")
-      break
-  else:
-    print(f"Congratulations, {name}!")
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break  
+
+    else:  
+        print(f"Congratulations, {name}!")
